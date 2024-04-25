6eb7584be09ee244308a4bf2fa14f9cd9d58207a
ozo-vehe <johnnie.vehe@gmail.com>
2024-04-26 00:55:11 +0100

    edited the index.ts and model.ts files

--- marketplace-contract/assembly/index.ts
+++ marketplace-contract/assembly/index.ts
@@ -1,4 +1,79 @@
-import { Product, listedProducts } from "./model";
+// import { Product, listedProducts } from "./model";
+// import { context, ContractPromiseBatch, u128 } from "near-sdk-as";
+
+// /**
+//  *
+//  * This function changes the state of data in the blockchain.
+//  * It is used to issue buy transactions when a product is purchased from a given seller (if the product is available)
+//  *
+//  * @param productId - an identifier of a product that is the subject of purchase
+//  */
+// export function placeBid(productId: string): void {
+//   const product = getProduct(productId);
+//     if (product == null) {
+//       throw new Error("product not found");
+//     }
+//     if (product.price.toString() > context.attachedDeposit.toString()) {
+//       throw new Error(
+//         "attached deposit should be greater than the product's price"
+//       );
+//     }
+//     if (product.sold) {
+//       throw new Error("product is already sold");
+//     }
+//     product.placeBid(context.attachedDeposit);
+// }
+
+// /**
+//  *
+//  * @param product - a product to be added to the blockchain
+//  */
+// export function setProduct(product: Product): void {
+//   let storedProduct = listedProducts.get(product.id);
+//   if (storedProduct !== null) {
+//     throw new Error(`a product with id=${product.id} already exists`);
+//   }
+//   listedProducts.set(product.id, Product.fromPayload(product));
+// }
+
+// /**
+//  *
+//  * A function that returns a single product for given owner and product id
+//  *
+//  * @param id - an identifier of a product to be returned
+//  * @returns a product for a given @param id
+//  */
+// export function getProduct(id: string): Product | null {
+//   return listedProducts.get(id);
+// }
+
+// /**
+//  *
+//  * A function that returns an array of products for all accounts
+//  *
+//  * @returns an array of objects that represent a product
+//  */
+// export function getProducts(): Array<Product> {
+//   return listedProducts.values();
+// }
+
+// /**
+//  *
+//  * A function to withdraw the highest bid.
+//  *
+//  * @returns an array of objects that represent a product
+//  */
+
+// export function withdrawBid(id: string): void {
+//   const product = getProduct(id);
+//   if (product == null) {
+//     throw new Error("Product not found");
+//   }
+//   product.withdrawBid();
+//   listedProducts.set(product.id, product);
+// }
+
+import { Product, productsStorage } from "./model";
 import { context, ContractPromiseBatch, u128 } from "near-sdk-as";
 
 /**
@@ -10,18 +85,27 @@ import { context, ContractPromiseBatch, u128 } from "near-sdk-as";
  */
 export function placeBid(productId: string): void {
   const product = getProduct(productId);
-    if (product == null) {
-      throw new Error("product not found");
-    }
-    if (product.price.toString() > context.attachedDeposit.toString()) {
-      throw new Error(
-        "attached deposit should be greater than the product's price"
-      );
-    }
-    if (product.sold) {
-      throw new Error("product is already sold");
-    }
-    product.placeBid(context.attachedDeposit);
+  // const sender = context.sender;
+  // const attachedDeposit = context.attachedDeposit;
+  if (product == null) {
+    throw new Error("product not found");
+  }
+  if (product.price.toString() != context.attachedDeposit.toString()) {
+    throw new Error(
+      "attached deposit should be greater than the product's price"
+    );
+  }
+  if (product.sold) {
+    throw new Error("product is already sold");
+  }
+  product.incrementSoldAmount();
+  if(product.bids.indexOf(context.attachedDeposit) > -1) {
+    const index = product.bids.indexOf(context.attachedDeposit);
+    product.bids[index] = context.attachedDeposit;
+  } else {
+    product.addBidders(context.sender);
+    product.addBids(context.attachedDeposit);
+  }
 }
 
 /**
@@ -29,11 +113,11 @@ export function placeBid(productId: string): void {
  * @param product - a product to be added to the blockchain
  */
 export function setProduct(product: Product): void {
-  let storedProduct = listedProducts.get(product.id);
+  let storedProduct = productsStorage.get(product.id);
   if (storedProduct !== null) {
     throw new Error(`a product with id=${product.id} already exists`);
   }
-  listedProducts.set(product.id, Product.fromPayload(product));
+  productsStorage.set(product.id, Product.fromPayload(product));
 }
 
 /**
@@ -44,7 +128,7 @@ export function setProduct(product: Product): void {
  * @returns a product for a given @param id
  */
 export function getProduct(id: string): Product | null {
-  return listedProducts.get(id);
+  return productsStorage.get(id);
 }
 
 /**
@@ -54,21 +138,5 @@ export function getProduct(id: string): Product | null {
  * @returns an array of objects that represent a product
  */
 export function getProducts(): Array<Product> {
-  return listedProducts.values();
-}
-
-/**
- *
- * A function to withdraw the highest bid.
- *
- * @returns an array of objects that represent a product
- */
-
-export function withdrawBid(id: string): void {
-  const product = getProduct(id);
-  if (product == null) {
-    throw new Error("Product not found");
-  }
-  product.withdrawBid();
-  listedProducts.set(product.id, product);
+  return productsStorage.values();
 }
--- marketplace-contract/assembly/model.ts
+++ marketplace-contract/assembly/model.ts
@@ -1,9 +1,88 @@
-import {
-  PersistentUnorderedMap,
-  u128,
-  context,
-  ContractPromiseBatch,
-} from "near-sdk-as";
+// import {
+//   PersistentUnorderedMap,
+//   u128,
+//   context,
+//   ContractPromiseBatch,
+// } from "near-sdk-as";
+
+// @nearBindgen
+// export class Product {
+//   id: string;
+//   name: string;
+//   description: string;
+//   image: string;
+//   price: u128;
+//   bidders: Array<string> = [];
+//   bids: Array<Object> = [{ bidder: String, bid: u128 }];
+//   bid: u128;
+//   bidder: string;
+//   duration: u64;
+//   owner: string;
+//   sold: bool;
+
+//   public static fromPayload(payload: Product): Product {
+//     const product = new Product();
+//     product.id = payload.id;
+//     product.name = payload.name;
+//     product.description = payload.description;
+//     product.image = payload.image;
+//     product.price = payload.price;
+//     product.owner = context.sender;
+//     product.duration = payload.duration;
+//     product.sold = payload.sold;
+//     product.bid = payload.bid;
+//     product.bidder = payload.bidder;
+//     product.bidders = payload.bidders;
+//     product.bids = payload.bids;
+//     return product;
+//   }
+
+//   // Change the current bid amount
+//   public placeBid(bid: u128): void {
+//     // Error checks
+//     if (this.sold) {
+//       throw new Error("Product is already sold");
+//     }
+//     if (this.owner == context.sender) {
+//       throw new Error("Seller cannot bid on their own product");
+//     }
+//     if (this.duration < context.blockTimestamp) {
+//       throw new Error("Auction has ended");
+//     }
+
+//     if (this.price < bid) {
+//       if (!this.bidders.includes(context.sender)) {
+//         this.bidders.push(context.sender);
+//       }
+//       // const bidderOject = { bidder: context.sender, bid: bid };
+//       this.bid = bid;
+//       this.bidder = context.sender;
+//     } else {
+//       throw new Error("Bid amount is lower than the current bid");
+//     }
+//   }
+
+//   // Function to withdraws winning bids
+//   public withdrawBid(): void {
+//     if (this.owner != context.sender) {
+//       throw new Error("Only the owner can withdraw the bid");
+//     }
+//     /*
+//     `ContractPromiseBatch` is used here to create a transaction to transfer the money to the seller
+//     The amount of money to be used in the transaction is taken from `context.attachedDeposit`
+//     which is defined by `--depositYocto=${AMOUNT}` parameter during the invocation
+//     */
+//     ContractPromiseBatch.create(this.owner).transfer(this.bid);
+//     this.owner = this.bidder;
+//     this.sold = true;
+//   }
+// }
+
+// export const listedProducts = new PersistentUnorderedMap<string, Product>(
+//   "LISTED_PRODUCTS"
+// );
+
+import { PersistentUnorderedMap, ContractPromiseBatch, u128, context } from "near-sdk-as";
 
 @nearBindgen
 export class Product {
@@ -11,14 +90,15 @@ export class Product {
   name: string;
   description: string;
   image: string;
+  location: string;
   price: u128;
-  bidders: Array<string> = [];
-  bids: { bidder: string; bid: u128 }[] = [];
-  bid: u128;
-  bidder: string;
-  duration: u64;
   owner: string;
   sold: bool = false;
+  duration: string;
+  bidders: Array<string> = [];
+  bids: Array<u128> = [];
+  bid: u128;
+  bidder: string;
 
   public static fromPayload(payload: Product): Product {
     const product = new Product();
@@ -26,46 +106,30 @@ export class Product {
     product.name = payload.name;
     product.description = payload.description;
     product.image = payload.image;
+    product.location = payload.location;
     product.price = payload.price;
     product.owner = context.sender;
     product.duration = payload.duration;
+
     return product;
   }
-
-  // Change the current bid amount
-  public placeBid(bid: u128): void {
-    // Error checks
-    if (this.sold) {
-      throw new Error("Product is already sold");
-    }
-    if (this.owner == context.sender) {
-      throw new Error("Seller cannot bid on their own product");
-    }
-    if (this.duration < context.blockTimestamp) {
-      throw new Error("Auction has ended");
-    }
-
-    if (this.price < bid) {
-      if (!this.bidders.includes(context.sender)) {
-        this.bidders.push(context.sender);
-      }
-      // const bidderOject = { bidder: context.sender, bid: bid };
-      this.bid = bid;
-      this.bidder = context.sender;
-    } else {
-      throw new Error("Bid amount is lower than the current bid");
-    }
+  public incrementSoldAmount(): void {
+    this.sold = true;
   }
-
-  // Function to withdraws winning bids
+  public addBidders(bidder: string): void {
+    this.bidders.push(bidder);
+  }
+  public addBids(bid: u128): void {
+    this.bids.push(bid);
+  }
   public withdrawBid(): void {
     if (this.owner != context.sender) {
       throw new Error("Only the owner can withdraw the bid");
     }
     /*
     `ContractPromiseBatch` is used here to create a transaction to transfer the money to the seller
-    The amount of money to be used in the transaction is taken from `context.attachedDeposit` 
-    which is defined by `--depositYocto=${AMOUNT}` parameter during the invocation 
+    The amount of money to be used in the transaction is taken from `context.attachedDeposit`
+    which is defined by `--depositYocto=${AMOUNT}` parameter during the invocation
     */
     ContractPromiseBatch.create(this.owner).transfer(this.bid);
     this.owner = this.bidder;
@@ -73,6 +137,6 @@ export class Product {
   }
 }
 
-export const listedProducts = new PersistentUnorderedMap<string, Product>(
+export const productsStorage = new PersistentUnorderedMap<string, Product>(
   "LISTED_PRODUCTS"
 );
